6997b8164b57263e68bce08f56ef91c215eb29be
Tim Smith <tsmith@llnw.com>
2013-02-12 10:12:35 -0800

    This cookbook supports older Ubuntu versions
    It had explicit support for 9.10 in the old defaults support.  There's
    no reason to state Ubuntu >= 10.04

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ recipe            "rsyslog", "Installs rsyslog"
 recipe            "rsyslog::client", "Sets up a client to log to a remote rsyslog server"
 recipe            "rsyslog::server", "Sets up an rsyslog server"
 
-supports          "ubuntu", ">= 10.04"
+supports          "ubuntu"
 supports          "debian", ">= 5.0"
 supports          "redhat", ">= 6.0"
 
