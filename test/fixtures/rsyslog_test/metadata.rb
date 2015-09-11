b1b9cb3a638dd7ccc291554463cf445bbd2c2f2f
Tim Smith <tsmith84@gmail.com>
2015-09-10 22:05:43 -0700

    Update URL

--- test/fixtures/rsyslog_test/metadata.rb
+++ test/fixtures/rsyslog_test/metadata.rb
@@ -1,6 +1,6 @@
 name              'rsyslog_test'
 maintainer        'Chef Software, Inc.'
-maintainer_email  'cookbooks@getchef.com'
+maintainer_email  'cookbooks@chef.io'
 license           'Apache 2.0'
 description       'Tests rsyslog cookbook'
 version           '1.0'
