efce081755f8c24e709dbe3b90e8ac4e0b5f3b84
Tim Smith <tsmith84@gmail.com>
2015-11-09 18:06:22 -0800

    Test relp so we don't break break it again

--- test/integration/relp/serverspec/relp_spec.rb
+++ test/integration/relp/serverspec/relp_spec.rb
@@ -7,3 +7,7 @@ end
 describe package('rsyslog-relp') do
   it { should be_installed }
 end
+
+describe file('/etc/rsyslog.d/49-remote.conf') do
+  its(:content) { should match /'*.* :omrelp:10.0.0.45:20514\n'*.* :omrelp:10.1.1.33:20514;RSYSLOG_SyslogProtocol23Format/ }
+end
