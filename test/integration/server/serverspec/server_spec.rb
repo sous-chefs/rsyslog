04aa7bcb51570d79abe6ace31f87b33b94c167f6
Martin Fenner <martin.fenner@datacite.org>
2016-01-04 00:17:04 +0100

    renamed remote.conf in tests

--- test/integration/server/serverspec/server_spec.rb
+++ test/integration/server/serverspec/server_spec.rb
@@ -8,6 +8,6 @@ describe file('/etc/rsyslog.d/35-server-per-host.conf') do
   it { should be_file }
 end
 
-describe file('/etc/rsyslog.d/remote.conf') do
+describe file('/etc/rsyslog.d/49-remote.conf') do
   it { should_not be_file }
 end
