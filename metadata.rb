7fb28df0c34fe852bb62e5ddcd506311a05b835f
Tim Smith <tsmith@chef.io>
2016-09-07 21:42:44 -0700

    Remove chef 11 compat
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -24,4 +24,4 @@ depends           'compat_resource', '>= 12.10'
 source_url 'https://github.com/chef-cookbooks/rsyslog'
 issues_url 'https://github.com/chef-cookbooks/rsyslog/issues'
 
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.1'
