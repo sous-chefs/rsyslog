0e963671886b1d7d7cfbe6605de715ee396ddc24
Tim Smith <tsmith@chef.io>
2018-01-16 17:56:47 -0800

    Simplify platform support in the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -10,14 +10,10 @@ recipe            'rsyslog', 'Sets up rsyslog for local logging'
 recipe            'rsyslog::client', 'Sets up a client to log to a remote rsyslog server'
 recipe            'rsyslog::server', 'Sets up an rsyslog server'
 
-supports          'ubuntu', '>= 12.04'
-supports          'debian', '>= 7.0'
-supports          'redhat', '>= 5.0'
-supports          'centos', '>= 5.0'
-supports          'fedora'
-supports          'scientific'
-supports          'amazon'
-supports          'oracle'
+
+%w(ubuntu debian mint redhat centos amazon scientific oracle fedora zlinux).each do |os|
+  supports os
+end
 
 source_url 'https://github.com/chef-cookbooks/rsyslog'
 issues_url 'https://github.com/chef-cookbooks/rsyslog/issues'
