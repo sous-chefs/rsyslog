fa3dd0b4d2173bf7987ec1cd6762d24b46607129
Ben Krueger <ben.krueger@nordstrom.com>
2018-07-03 17:21:18 -0700

    #146 Exclude Amazon Linux from old versions check

--- attributes/default.rb
+++ attributes/default.rb
@@ -151,6 +151,6 @@ else
 end
 
 # rsyslog 3/4 do not support the new :omusrmsg:* format and need * instead
-if node['platform_family'] == 'rhel' && node['platform_version'].to_i < 6
+if node['platform'] != 'amazon' && node['platform_family'] == 'rhel' && node['platform_version'].to_i < 6
   default['rsyslog']['default_facility_logs']['*.emerg'] = '*'
 end
