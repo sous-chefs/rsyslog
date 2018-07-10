011016e8fa23ab1229006f9c5290bead994365d9
Tim Smith <tsmith@chef.io>
2018-07-10 12:02:42 -0700

    Release 6.0.5
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Installs and configures rsyslog'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '6.0.4'
+version           '6.0.5'
 
 recipe            'rsyslog', 'Sets up rsyslog for local logging'
 recipe            'rsyslog::client', 'Sets up a client to log to a remote rsyslog server'
