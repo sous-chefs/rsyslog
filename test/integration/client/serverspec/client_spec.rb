20cd8499f4dd52a073be00755f5f16c02e6578b5
Kyle McGovern <kyle.mcgovern@cerner.com>
2018-08-13 13:48:50 -0500

    fixup cookstyle
    Signed-off-by: Kyle McGovern <kyle.mcgovern@cerner.com>

--- test/integration/client/serverspec/client_spec.rb
+++ test/integration/client/serverspec/client_spec.rb
@@ -10,7 +10,7 @@ end
 
 describe file('/etc/rsyslog.d/49-remote.conf') do
   it { should be_file }
-  its(:content) { should match "@@10.0.0.50:514" }
+  its(:content) { should match '@@10.0.0.50:514' }
   its(:content) { should match /^\$ActionQueueMaxDiskSpace 1G/ }
   its(:content) { should match /authpriv\.\*,cron\.\*,daemon\.\* @@10\.1\.1\.33:654/ }
   its(:content) { should match /auth\.\*,mail\.\* @10.0.0.45:555/ }
