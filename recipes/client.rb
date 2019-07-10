99d30688657917b1fc6193d8b9432ed67ddb49d8
Hamish Forbes <hamish.forbes@bglgroup.co.uk>
2019-07-10 11:50:02 +0100

    removing reliance on search to remove chef confusiong on solo vs server

--- recipes/client.rb
+++ recipes/client.rb
@@ -21,16 +21,7 @@
 return if node['rsyslog']['server']
 include_recipe 'rsyslog::default'
 
-results = search(:node, node['rsyslog']['server_search']).map do |server|
-  ipaddress = server['ipaddress']
-  # If both server and client are on the same cloud and local network, they may be
-  # instructed to communicate via the internal interface by enabling `use_local_ipv4`
-  if node['rsyslog']['use_local_ipv4'] && server.attribute?('cloud') && server['cloud']['local_ipv4']
-    ipaddress = server['cloud']['local_ipv4']
-  end
-  ipaddress
-end
-server_ips = Array(node['rsyslog']['server_ip']) + Array(results)
+server_ips = Array(node['rsyslog']['server_ip'])
 
 rsyslog_servers = []
 
